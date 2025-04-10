a385b9ce2f1b168624f9ba6d8bdafb49bb93eb30
ranasohaib079 <sohaibrana079@gmail.com>
2025-04-09 20:34:49 -0500

    Initial Commit

---
+++ FetchRecipeApp/FetchRecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipeAppApp.swift
+//  FetchRecipeApp
+//
+//  Created by Sohaib Rana on 4/9/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FetchRecipeAppTests/FetchRecipeAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  FetchRecipeAppTests.swift
+//  FetchRecipeAppTests
+//
+//  Created by Sohaib Rana on 4/9/25.
+//
+
+import Testing
+
+struct FetchRecipeAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
