f804de5d5d64b8185980fc929b1695d57ac11b0d
ranasohaib079 <sohaibrana079@gmail.com>
2025-04-11 18:58:50 -0500

    Add YouTube video functionality and update RecipeRowView

--- FetchRecipeApp/Views/RecipeRowView.swift
+++ FetchRecipeApp/Views/RecipeRowView.swift
@@ -8,6 +8,7 @@
 /**
  This view represents a single row in the recipe list which displays essential recipe information such as the
  recipe's name, the cuisine type, and a thumbnail image loaded asynchronously via CachedAsyncImage.
+ There is also a "View Recipe" link that takes the user to a YouTube video of the recipe.
  */
 
 import SwiftUI
@@ -16,27 +17,35 @@ struct RecipeRowView: View {
     let recipe: Recipe
     
     var body: some View {
-        HStack(spacing: 12) {
-            if let url = recipe.photoURLSmall {
-                CachedAsyncImage(url: url)
-                    .frame(width: 80, height: 80)
-                    .cornerRadius(8)
-                    .clipped()
-            } else {
-                Rectangle()
-                    .fill(.gray.opacity(0.3))
-                    .frame(width: 80, height: 80)
-                    .cornerRadius(8)
+        VStack(alignment: .leading, spacing: 8) {
+            HStack(spacing: 12) {
+                if let url = recipe.photoURLSmall {
+                    CachedAsyncImage(url: url)
+                        .frame(width: 80, height: 80)
+                        .cornerRadius(8)
+                        .clipped()
+                } else {
+                    Rectangle()
+                        .fill(Color.gray.opacity(0.3))
+                        .frame(width: 80, height: 80)
+                        .cornerRadius(8)
+                }
+                
+                VStack(alignment: .leading, spacing: 4) {
+                    Text(recipe.name)
+                        .font(.headline)
+                    Text(recipe.cuisine)
+                        .font(.subheadline)
+                        .foregroundColor(.secondary)
+                }
             }
             
-            VStack(alignment: .leading, spacing: 4) {
-                Text(recipe.name)
-                    .font(.headline)
-                Text(recipe.cuisine)
-                    .font(.subheadline)
-                    .foregroundColor(.secondary)
+            if let youtubeURL = recipe.youtubeURL {
+                Link("View Recipe", destination: youtubeURL)
+                    .font(.caption)
+                    .foregroundColor(.blue)
             }
         }
+        .padding(.vertical, 4)
     }
 }
-
